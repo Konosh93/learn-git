4d14f4ce895aef3f8a980969ec347aedc7412888
Omer <omer.rayah@gmail.com>
2017-12-05 15:59:43 +0900

    Add the first react component App to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ECHO is on.
+var App = React.createClass({
+  displayName: 'App',
+  render: function() {
+    // The second parameter is an object of attributes for the element (if any)
+    return React.createElement('div', { }, 'Something something Dark Side');
+  }
+});
+ 
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('root')
+);
